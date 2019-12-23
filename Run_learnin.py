d80f2befd5c16efdd6a2a644a60310f65bd9f551
youngjaean <yous4831@hanmail.net>
2019-12-23 14:58:24 +0900

    변경사항

--- Run_learnin.py
+++ Run_learnin.py
@@ -10,32 +10,32 @@ from Optima import NoamOpt
 from config import batch_size_fn, run_epoch
 from torchtext import data, datasets
 
-if True:
-    import spacy
-    spacy_de = spacy.load('de')
-    spacy_en = spacy.load('en')
+# if True:
+#     import spacy
+#     spacy_de = spacy.load('de')
+#     spacy_en = spacy.load('en')
 
-    def tokenize_de(text):
-        return [tok.text for tok in spacy_de.tokenizer(text)] #spacy를 이용
+#     def tokenize_de(text):
+#         return [tok.text for tok in spacy_de.tokenizer(text)] #spacy를 이용
 
-    def tokenize_en(text):
-        return [tok.text for tok in spacy_en.tokenizer(text)]
+#     def tokenize_en(text):
+#         return [tok.text for tok in spacy_en.tokenizer(text)]
 
-    BOS_WORD = '<s>'
-    EOS_WORD = '</s>'
-    BLANK_WORD = "<blank>"
-    SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD) #tensor로 표시 할 수있는 공통 텍스트 처리 데이터 유형을 모델링
-    TGT = data.Field(tokenize=tokenize_en, init_token = BOS_WORD, 
-                     eos_token = EOS_WORD, pad_token=BLANK_WORD)
+#     BOS_WORD = '<s>'
+#     EOS_WORD = '</s>'
+#     BLANK_WORD = "<blank>"
+#     SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD) #tensor로 표시 할 수있는 공통 텍스트 처리 데이터 유형을 모델링
+#     TGT = data.Field(tokenize=tokenize_en, init_token = BOS_WORD, 
+#                      eos_token = EOS_WORD, pad_token=BLANK_WORD)
 
-    MAX_LEN = 100
-    train, val, test = datasets.IWSLT.splits(
-        exts=('.de', '.en'), fields=(SRC, TGT), 
-        filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and 
-            len(vars(x)['trg']) <= MAX_LEN)
-    MIN_FREQ = 2
-    SRC.build_vocab(train.src, min_freq=MIN_FREQ)
-    TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+#     MAX_LEN = 100
+#     train, val, test = datasets.IWSLT.splits(
+#         exts=('.de', '.en'), fields=(SRC, TGT), 
+#         filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and 
+#             len(vars(x)['trg']) <= MAX_LEN)
+#     MIN_FREQ = 2
+#     SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+#     TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
 
 class MyIterator(data.Iterator):
     def create_batches(self):
@@ -195,4 +195,4 @@ print(trans)
 
 import fire
 if __name__ == '__main__':
-    fire.Fire(main)
+    fire.Fire()
--- subsequent_mask.py
+++ subsequent_mask.py
@@ -4,5 +4,5 @@ import torch
 def subsequent_mask(size):
     "Mask out subsequent positions."
     attn_shape = (1, size, size)
-    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')#1채운 배열 마지막 삼각형을 0으로 채운다 
     return torch.from_numpy(subsequent_mask) == 0
