573ab22424b41a44b46b078986bbd37b479574ce
youngjaean <yous4831@hanmail.net>
2020-01-08 12:34:46 +0900

    변경

--- data.py
+++ data.py
@@ -1,14 +1,62 @@
-import numpy as np
-import torch 
-from torch.autograd import Variable
-from make_modle import Batch
-# def data_gen(V, batch, nbatches):
-#     "Generate random data for a src-tgt copy task."
-#     for i in range(nbatches):
-#         data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
-#         data[:, 0] = 1
-#         src = Variable(data, requires_grad=False)
-#         tgt = Variable(data, requires_grad=False)
-#         yield Batch(src, tgt, 0)
+import sys
+sys.path.append('..')
+import os, argparse, datetime, re, collections
+from tqdm import tqdm, trange #진행 바 
+import json
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
+class QA_data_set(torch.utils.data.Dataset):
+    def __init__(self, vocab, infile):
+        self.vocab = vocab
+        self.labels = []
+        self.sentences = []
+
+        line_cnt = 0
+        with open(infile, "r") as f:
+            for line in f:
+                line_cnt += 1
+
+        with open(infile, "r") as f:
+            for i, line in enumerate(tqdm(f, total=line_cnt, desc=f"Loading {infile}", unit=" lines")):
+                data = json.loads(line)
+                self.labels.append(data["label"]) #데이터셋 정제가 문제 
+                self.sentences.append([vocab.piece_to_id(p) for p in data["doc"]])
+    
+    def __len__(self):
+        assert len(self.labels) == len(self.sentences)
+        return len(self.labels)
+    
+    def __getitem__(self, item):
+        return (torch.tensor(self.labels[item]),
+                torch.tensor(self.sentences[item]),
+                torch.tensor([self.vocab.piece_to_id("[BOS]")]))
+    
+
+def QA_collate_fn(inputs):    
+    labels, enc_inputs, dec_inputs = list(zip(*inputs))
+
+    enc_inputs = torch.nn.utils.rnn.pad_sequence(enc_inputs, batch_first=True, padding_value=0)
+    dec_inputs = torch.nn.utils.rnn.pad_sequence(dec_inputs, batch_first=True, padding_value=0)
+
+    batch = [
+        torch.stack(labels, dim=0),
+        enc_inputs,
+        dec_inputs,
+    ]
+    return batch
+
+
+""" 데이터 로더 """
+def build_data_loader(vocab, infile, args, shuffle=True):
+    dataset = QA_data_set(vocab, infile)
+    if 1 < args.n_gpu and shuffle:
+        sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch, sampler=sampler, collate_fn=QA_collate_fn)
+    else:
+        sampler = None
+        loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch, sampler=sampler, shuffle=shuffle, collate_fn=QA_collate_fn)
+    return loader, sampler
